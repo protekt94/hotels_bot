a8d118892b54ba6c64c5355b5cc53a9eca2b90b3
uryvko.aa <uryvko.aa@gmail.com>
2022-10-03 14:42:45 +0300

    version 0.7

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,31 +1,38 @@
 import sqlite3
+import psycopg2
 
 
 class BotDB:
 
-    def __init__(self, db_file):
-        self.conn = sqlite3.connect(db_file)
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            database='d32hs4fk6f1rcv',
+            user="ankamqkptqxuqq",
+            password="782dcdd9b54c1d81dbd622fa7326703d2cfe16acaa2a4d69c07e9d385889c50d",
+            host="ec2-34-252-216-149.eu-west-1.compute.amazonaws.com",
+            port="5432"
+        )
         self.cursor = self.conn.cursor()
 
     def user_exists(self, user_id):
         """Проверяем, есть ли юзер в базе"""
-        result = self.cursor.execute("SELECT `id` FROM `users` WHERE `user_id` = ?", (user_id,))
-        return bool(len(result.fetchall()))
+        self.cursor.execute("SELECT id FROM users WHERE user_id = %s", (user_id,))
+        return bool(len(self.cursor.fetchall()))
 
     def get_user_id(self, user_id):
         """Достаем id юзера в базе по его user_id"""
-        result = self.cursor.execute("SELECT `id` FROM `users` WHERE `user_id` = ?", (user_id,))
-        return result.fetchone()[0]
+        self.cursor.execute("SELECT id FROM users WHERE user_id = %s", (user_id,))
+        return self.cursor.fetchone()[0]
 
     def add_user(self, user_id):
         """Добавляем юзера в базу"""
-        self.cursor.execute("INSERT INTO `users` (`user_id`) VALUES (?)", (user_id,))
+        self.cursor.execute("INSERT INTO users (user_id) VALUES (%s)", (user_id,))
         return self.conn.commit()
 
     def add_record(self, user_id, hotel_name, rating, price, photos):
         """Создаем запись о доходах/расходах"""
-        self.cursor.execute("INSERT INTO `records` (`user_id`, `hotel_name`, `rating`,'price', 'photos') VALUES (?, ?, "
-                            "?, ?, ?)",
+        self.cursor.execute("INSERT INTO records (user_id, hotel_name, rating,price, photos) VALUES (%s, %s, "
+                            "%s, %s, %s)",
                             (self.get_user_id(user_id), hotel_name, rating, price, photos))
 
         return self.conn.commit()
@@ -33,12 +40,12 @@ class BotDB:
     def get_records(self, user_id):
         """Получаем историю о доходах/расходах"""
 
-        result = self.cursor.execute("SELECT * FROM `records` WHERE `user_id` = ?", (self.get_user_id(user_id),))
-        return result.fetchall()
+        self.cursor.execute("SELECT * FROM records WHERE user_id = %s", (self.get_user_id(user_id),))
+        return self.cursor.fetchall()
 
     def get_del_records(self, user_id):
         print(self.get_user_id(user_id))
-        self.cursor.execute("""DELETE FROM records WHERE user_id = ?""", (self.get_user_id(user_id),))
+        self.cursor.execute("""DELETE FROM records WHERE user_id = %s""", (self.get_user_id(user_id),))
         return self.conn.commit()
 
     def close(self):
